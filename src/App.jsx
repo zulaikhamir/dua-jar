f77d13ff592b22431d6bb76cdb9948ba1c89917e
zulaikhamir <zulaikhamir786@gmail.com>
2025-03-26 17:55:39 +0530

    Pages Linked

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import VirtualDuaJar from "./components/Jar";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Home() {
+  let navigate = useNavigate();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container text-center mt-5">
+      <h1 className="display-4 text-primary">Don't be sad!</h1>
+      <button className="btn btn-success" onClick={() => navigate("/jar")}>
+        Click Me
+      </button>
+    </div>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Router basename="/dua-jar">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jar" element={<VirtualDuaJar />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
